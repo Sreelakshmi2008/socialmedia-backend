05b10beb8d85135b5be1e4526f19d3ccb0cc7968
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-12-29 11:56:18 +0530

    again chsnges

--- authentication/models.py
+++ authentication/models.py
@@ -19,7 +19,7 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15, unique=True,blank=True,null=True)
+    phone = models.CharField(max_length=15,blank=True,null=True)
     profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     
     name = models.CharField(max_length=255, blank=True)
